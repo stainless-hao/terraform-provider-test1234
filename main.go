75a4dc4220aa300b85575cf902e8b2eb74b210f9
hao <hao@stainlessapi.com>
2024-07-10 12:37:26 -0400

    init

---
+++ main.go
@@ -0,0 +1,26 @@
+// ; exec go run "$0" "$@"
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+)
+
+func init() {
+	log.SetFlags(log.Lshortfile)
+}
+
+func main() {
+	self, err := os.Executable()
+	if err != nil {
+		log.Panicln(err)
+	}
+	dir := filepath.Dir(self)
+	if err := os.Chdir(dir); err != nil {
+		log.Panicln(err)
+	}
+	fmt.Println(dir)
+
+}
